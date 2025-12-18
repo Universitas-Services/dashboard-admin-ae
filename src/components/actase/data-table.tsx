c688e917499663c4851a7b2eced3108595452a9d
Yovani Navas <yovaninavas10@gmail.com>
2025-12-18 09:15:01 -0400

    fix en error de etiqueta de navegacion en data-table

--- src/components/actase/data-table.tsx
+++ src/components/actase/data-table.tsx
@@ -37,6 +37,7 @@ export function DataTable<TData, TValue>({
   isLoading = false,
 }: DataTableProps<TData, TValue>) {
   
+  // eslint-disable-next-line react-hooks/incompatible-library
   const table = useReactTable({
     data,
     columns,
